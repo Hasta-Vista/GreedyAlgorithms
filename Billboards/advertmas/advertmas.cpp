8129f355ee3d090a69b64028a7773cd35b99c1c5
Hasta-Vista <ovchinnikov675@yandex.com>
2024-08-25 23:53:08 +0400

    repos3

---
+++ Billboards/advertmas/advertmas.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int billboards, advertisers, weeks, price, lengthWeek;
+    int max_profit = 0;
+    cin >> billboards >> advertisers >> weeks;
+    vector < pair<int, int> > vec;
+    for (int i = 0; i != advertisers; ++i) {
+         if (cin >> price >> lengthWeek) {
+            vec.push_back(make_pair(price, lengthWeek));
+        }
+    }
+    sort(vec.rbegin(), vec.rend());
+    for (int i = 0; i != weeks; ++i) {
+        for (int j = 0; j != billboards; ++j) {
+            for (int a = 0; a != vec.size(); ++a) {
+                if (price != 0) {
+                    max_profit += lengthWeek;
+                    if (price != 0) {
+                        price--;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    cout << max_profit << endl;
+    vec.clear();
+    return 0;
+}
+
+
